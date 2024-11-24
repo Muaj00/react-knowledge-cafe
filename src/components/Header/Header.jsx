45e4c1b69ffdf9a4523c105fe6dd871aa6d9d101
Muaj00 <muaj15-14389@diu.edu.bd>
2024-11-24 13:51:10 +0600

    create fake Json data and load them

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl text-center bg-orange-400'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
